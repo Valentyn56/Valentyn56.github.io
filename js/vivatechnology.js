a29f2a8686e7ef747c5175104c0d4f684e138e84
Valentyn56 <valentyn56@hotmail.fr>
2021-02-17 21:41:11 +0100

    Design burger menu edited

--- js/vivatechnology.js
+++ js/vivatechnology.js
@@ -3,16 +3,16 @@ document.getElementsByClassName("app-header")[0].setAttribute("id", "Appheader")
 
 var page = document.getElementById('Scrollcontent');
 var nav = document.getElementById("Appheader");
-
+var burgermenu = document.getElementById("burger-menu");
 
 page.addEventListener("scroll", function(){
     var pageScroll = page.scrollTop;
 
     if(pageScroll >= 10){
-
         nav.classList.add("scrolled");
+        burgermenu.classList.add("scrolled");
     }else{
-
         nav.classList.remove("scrolled");
+        burgermenu.classList.remove("scrolled");
     }
 });
