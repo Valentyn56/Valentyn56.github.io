ae43c00a91508b68c109a68856ab429fdd1313d5
Valentyn56 <valentyn56@hotmail.fr>
2021-02-17 19:04:23 +0100

    Javascript edited

--- js/vivatechnology.js
+++ js/vivatechnology.js
@@ -14,10 +14,13 @@ window.onscroll = function () {
 */
 
 window.addEventListener('scroll', function (e) {
+    console.log("ok")
     var nav = document.getElementsByClassName('app-header');
     if (document.documentElement.scrollTop || document.body.scrollTop > 10) {
+        console.log("cocou");
         nav.classList.add('scrolled');
     } else {
         nav.classList.remove('scrolled');
+        console.log("pas coucou");
     }
 });
