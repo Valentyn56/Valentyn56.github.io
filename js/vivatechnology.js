286887d41f4de0f71c38a67286c087a8896b8538
Valentyn56 <valentyn56@hotmail.fr>
2021-02-18 12:16:43 +0100

    js reponsive edited

--- js/vivatechnology.js
+++ js/vivatechnology.js
@@ -5,14 +5,20 @@ var page = document.getElementById('Scrollcontent');
 var nav = document.getElementById("Appheader");
 var burgermenu = document.getElementById("burger-menu");
 
-page.addEventListener("scroll", function(){
-    var pageScroll = page.scrollTop;
+if(document.getElementsByClassName("app-page")[0].classList.contains("homepage")){
+    page.addEventListener("scroll", function(){
+        var pageScroll = page.scrollTop;
 
-    if(pageScroll >= 10){
-        nav.classList.add("scrolled");
-        burgermenu.classList.add("scrolled");
-    }else{
-        nav.classList.remove("scrolled");
-        burgermenu.classList.remove("scrolled");
-    }
-});
+        if(pageScroll >= 10){
+            nav.classList.add("scrolled");
+            burgermenu.classList.add("scrolled");
+        }else{
+            nav.classList.remove("scrolled");
+            burgermenu.classList.remove("scrolled");
+        }
+    });
+}else{
+    nav.classList.add("scrolled");
+    burgermenu.classList.add("scrolled");
+}
+
